9265c4c94d5ed8fdaa2db1fafc8b085e128b9c5b
Lagadapati Sai Nagarjuna <lagadapati.nagarjuna2007@gmail.com>
2026-04-19 21:30:21 +0530

    Add CORS for Netlify domain

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const { sendMonthlyReport } = require('./telegramReport');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://smart-canteengo.netlify.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true
+}));
 
 const PORT = process.env.PORT || 3000;
 
